d26adf394f5cc6df691013571b91220c03d0222a
ranelya <ranelakarymsakova5@gmail.com>
2024-01-27 22:36:55 +0600

    menu

--- core.js
+++ core.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.querySelector('.menu-icon');
-  const navList = document.querySelector('.nav-main_list');
+const burger = document?.querySelector('[data-burger]')
+const nav = document?.querySelector('[data-nav]')
+const body = document?.body;
 
-  menuIcon.addEventListener('click', function () {
-      navList.classList.toggle('show');
-  });
-
-  // Close the menu when a link is clicked
-  const navLinks = document.querySelectorAll('.nav-main_link');
-  navLinks.forEach(function (link) {
-      link.addEventListener('click', function () {
-          navList.classList.remove('show');
-      });
-  });
-});
+burger?.addEventListener('click', () =>{
+    body.classList.toggle('stop-scroll')
+    burger?.classList.toggle('burger--active');
+    nav?.classList.toggle('nav--visible');
+})
